601e9a1eaafd68a819f38709bc786da7cd78c36e
Sharon Kamal Kothapalli <sharonkamalkothapalli@gmail.com>
2024-06-04 23:24:32 -0500

    removing fragment and using only <>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import ListGroup from "./components/ListGroup";
+// import { Fragment } from "react/jsx-runtime";
 
 function App() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
