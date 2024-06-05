8b3453b06253ee635472c09e45875ef27563a492
Sharon Kamal Kothapalli <sharonkamalkothapalli@gmail.com>
2024-06-05 00:01:38 -0500

    rendering lists

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,9 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
-    <>
-      <h1>List</h1>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
-    </>
+    <div>
+      <ListGroup />
+    </div>
   );
 }
 
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,30 @@
-function ListGroup(){
-    return <h1>List Group</h1>;
+function ListGroup() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null;
+  // };
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
